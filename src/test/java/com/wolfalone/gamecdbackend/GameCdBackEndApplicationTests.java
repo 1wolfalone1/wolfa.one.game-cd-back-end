ee49f477e25da07ab4a7885cc73b8e485e7f549d
NNhatTThien <thientryhard@gmail.com>
2023-07-03 11:00:09 +0700

    f

--- src/test/java/com/wolfalone/gamecdbackend/GameCdBackEndApplicationTests.java
+++ src/test/java/com/wolfalone/gamecdbackend/GameCdBackEndApplicationTests.java
@@ -1,13 +1,13 @@
-package com.wolfalone.gamecdbackend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GameCdBackEndApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.wolfalone.gamecdbackend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GameCdBackEndApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
--- src/test/java/com/wolfalone/gamecdbackend/repository/AccountRepoTest.java
+++ src/test/java/com/wolfalone/gamecdbackend/repository/AccountRepoTest.java
@@ -1,22 +1,22 @@
-package com.wolfalone.gamecdbackend.repository;
-
-import com.wolfalone.gamecdbackend.entity.Account;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class AccountRepoTest {
-    @Autowired
-    private AccountRepo accountRepo;
-
-    @Test
-    public void fetchAll() {
-        long a = accountRepo.countByEmailAndActive("");
-        System.out.println(a);
-    }
-}
+//package com.wolfalone.gamecdbackend.repository;
+//
+//import com.wolfalone.gamecdbackend.entity.Account;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class AccountRepoTest {
+//    @Autowired
+//    private AccountRepo accountRepo;
+//
+//    @Test
+//    public void fetchAll() {
+//        long a = accountRepo.countByEmailAndActive("");
+//        System.out.println(a);
+//    }
+//}
